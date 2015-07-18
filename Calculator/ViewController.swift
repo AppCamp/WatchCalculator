97f3a467c552e5ca659aafd51d29baaa6d4cbfdc
Marta Wozniak <M.Wozniak@kainos.com>
2015-07-18 15:35:40 +0200

    Calculator without MVC

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var firstDigit: Bool = true
+    var operand1: Double = 0
+    var operation: String = "="
+    
+    var displayValue: Double {
+        get {
+            return (display.text! as NSString).doubleValue
+        }
+        
+        set{
+            display.text = "\(newValue)"
+        }
+    }
+    
+    var isUserInTheMiddleOfTypingNumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if isUserInTheMiddleOfTypingNumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            isUserInTheMiddleOfTypingNumber = true
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func performOperation(sender: UIButton) {
+        firstDigit = false
+        isUserInTheMiddleOfTypingNumber = false
+        operation = sender.currentTitle!
+        operand1 = displayValue
     }
-
-
+    
+    @IBAction func calculate(sender: UIButton) {
+        switch operation{
+            case "+": displayValue = displayValue + operand1
+            case "-": displayValue = operand1 - displayValue
+            default: displayValue = 0
+        }
+        
+        isUserInTheMiddleOfTypingNumber = false
+        
+    }
 }
 
